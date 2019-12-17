b08e0cb6df5b87cb76c15ae3bea5714a410046e4
Wayne Reeves <wreeves@lifepathsystems.org>
2019-12-17 13:22:09 -0600

    a little shorter height for small window

--- Check-Password.ps1
+++ Check-Password.ps1
@@ -8,7 +8,7 @@ param(
 [xml]$XAMLsmall = @'
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="MainWindow" Height="300" Width="900"
+        Title="MainWindow" Height="260" Width="900"
 	WindowStartupLocation="CenterScreen"
         WindowStyle="None"
         BorderBrush="#b81237"
