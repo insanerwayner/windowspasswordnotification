c20f41d13d2f64b044adab22e9ac4c87ac05093a
Wayne Reeves <wreeves@lifepathsystems.org>
2019-12-18 13:16:29 -0600

    formatted the class in Get-DPISetting to my style and got rid of whitespace before string termination

--- Check-Password.ps1
+++ Check-Password.ps1
@@ -190,30 +190,33 @@ Function Load-XAML
 # Region GetDPISetting
 Function Get-DPISetting
     {
-    Add-Type @'
-      using System;
-      using System.Runtime.InteropServices;
-      using System.Drawing;
+    Add-Type `
+@'
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Drawing;
 
-      public class DPI {
+    public class DPI 
+	{
 	[DllImport("gdi32.dll")]
 	static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
 
-	public enum DeviceCap {
-	  VERTRES = 10,
-	  DESKTOPVERTRES = 117
-	}
+	public enum DeviceCap 
+	    {
+	    VERTRES = 10,
+	    DESKTOPVERTRES = 117
+	    }
 
-	public static float scaling() {
-	  Graphics g = Graphics.FromHwnd(IntPtr.Zero);
-	  IntPtr desktop = g.GetHdc();
-	  int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
-	  int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
-
-	  return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+	public static float scaling() 
+	    {
+	    Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+	    IntPtr desktop = g.GetHdc();
+	    int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+	    int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+	    return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+	    }
 	}
-      }
-    '@ -ReferencedAssemblies 'System.Drawing.dll'
+'@ -ReferencedAssemblies 'System.Drawing.dll'
 
     $Scale = [Math]::round([DPI]::scaling(), 2) * 100
     return $Scale
