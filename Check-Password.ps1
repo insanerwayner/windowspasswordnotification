be1f61fc2e746d0dd3f13743b14bd32afa80074e
Wayne Reeves <wreeves@lifepathsystems.org>
2019-12-19 13:49:20 -0600

    trying inputobject versus argumentlist for the job

--- Check-Password.ps1
+++ Check-Password.ps1
@@ -275,7 +275,7 @@ catch
 if ( $timeleft -le $DaysToStart )
 	{
 	$Scale = Get-DPISetting
-	Start-Job -InitializationScript $InitializationScript -ScriptBlock { Check-IfChanged -LockScreenOnPasswordChange $args } -Name CheckIfChanged -ArgumentList $LockScreenOnPasswordChange
+	Start-Job -InitializationScript $InitializationScript -ScriptBlock { Check-IfChanged -LockScreenOnPasswordChange $input } -Name CheckIfChanged -InputObject $LockScreenOnPasswordChange
 	Load-XAML -Days $timeleft -Scale $Scale
 	}
 else
