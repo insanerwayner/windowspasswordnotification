c4c254e7c41c9274c7d97fae24709c527432734e
Wayne Reeves <wreeves@lifepathsystems.org>
2019-12-31 10:45:58 -0600

    finished help file

--- help.ps1
+++ help.ps1
@@ -1,25 +1,45 @@
 <#
 .SYNOPSIS
-  <Overview of script>
+
+    Check AD user's password expiration status and prompt the user to change if needed.
 
 .DESCRIPTION
-  <Brief description of script>
+    
+    Displays a notification window for the user indicating that their password is about to or is expired, if within the defined parameters. It bases the expiration off of the domain password policy. The window notification gives instructions to user on how to change their password and gives them the choice to wait for the password change and make the window go away. On the last day remaining the choice to postpone will be removed, forcing the user to change the password to remove the window. The window will disappear automatically if it detects the user has changed their password.
 
-.PARAMETER <Parameter_Name>
-    <Brief description of parameter input required. Repeat this attribute if required>
+.PARAMETER DaysToStart
 
-.INPUTS
-  <Inputs if any, otherwise state None>
+    The DaysToStart parameter specifies when to fist display the window based on how many days left until their password will expire.  
 
-.OUTPUTS
-  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+.PARAMETER DaysToMaximizeWindow
+    
+    The DaysToMaximizeWindow specifies when to maximize the notification window to fullscreen based on how many days left until their password will expire.
+
+.PARAMETER LockScreenOnPasswordChange
+
+    The LockScreenOnPasswordChange will lock the screen after the user has changed their password, forcing them to re-enter the new password they created.
 
 .NOTES
-  Version:        1.0
-  Author:         <Name>
-  Creation Date:  <Date>
-  Purpose/Change: Initial script development
   
+  Author: Wayne Reeves
+
 .EXAMPLE
-  <Example goes here. Repeat this attribute for more than one example>
+
+  Check-Password
+
+  Description: Will run with default values with no parameters specified. The window will not pop up until the password will expire at a maximum of 7 days. The window will not maximize to Fullscreen until a maximum of 3 days remaining.
+
+ 
+.EXAMPLE
+
+  Check-Password -DaysToStart 4 -DaysToMaximizeWindow 2
+
+  Description: The window will not pop up until the password will expire at a maximum of 4 days. The window will not maximize to Fullscreen until a maximum of 2 days remaining.
+
+.EXAMPLE
+
+  Check-Password -DaysToStart 6 -DaysToMaximizeWindow 3 -LockScreenOnPasswordChange
+
+  Description: The window will not pop up until the password will expire at a maximum of 6 days. The window will not maximize to Fullscreen until a maximum of 3 days remaining. Once the user has changed their password it will lock their workstation, forcing them to sign back in with their newly created password.
+
 #>
